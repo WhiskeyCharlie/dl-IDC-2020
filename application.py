262701b8a3bef9320f849f654b6460423f281e2a
Jonathan Simon <jnthnsimon@gmail.com>
2021-02-16 13:34:19 +0200

    WORKING VERY SIMPLE VERSION

--- application.py
+++ application.py
@@ -1,38 +1,54 @@
+from typing import Union
+
 from model import UNET
 from training import IMAGE_SIZE
 from PIL import Image
 import torch
-import torchvision
+from torchvision import transforms
 import matplotlib.pyplot as plt
-from time import time
+import pyfakewebcam
+import cv2
+import time
 import numpy as np
+
+WEBCAM_OUTPUT_RESOLUTION = (640, 480)
 
 
 def _normalize_image_to_tensor(img: Image):
     resized_img = img.resize((IMAGE_SIZE, IMAGE_SIZE))
-    return torchvision.transforms.PILToTensor()(resized_img).float() / 255
+    return transforms.PILToTensor()(resized_img).float() / 255
 
 
-def remove_background_single_image(src_image: str, model: UNET) -> Image:
-    img = Image.open(src_image)
+def remove_background_single_image(src_image: Union[str, Image.Image], model: UNET) -> Image:
+    if type(src_image) == str:
+        img = Image.open(src_image)
+    else:
+        img = src_image
     img_tensor = _normalize_image_to_tensor(img).unsqueeze(0)
     mask = torch.gt(model(img_tensor).squeeze(0), 0.5)
-    mask_img = torchvision.transforms.ToPILImage()(mask.int() * 255).convert('L').resize(img.size,
-                                                                                         resample=Image.BICUBIC)
-    background = Image.new('RGBA', img.size, 'BLACK')
+    mask_img = transforms.ToPILImage()(mask.int() * 255).convert('L').resize(img.size, resample=Image.BICUBIC)
+    background = Image.new('RGBA', img.size, (0, 255, 0))
     img.convert('RGBA')
-    img.putalpha(mask_img, )
-    print(np.asarray(img))
+    result = Image.composite(img, background, mask_img)
     background.paste(img)
-    return background
+    return result.convert('RGB')
 
 
 def main():
     model = UNET(3, 1)
     model.load_state_dict(torch.load('./saved_models/unet.pt'))
     model.eval()
-    plt.imshow(remove_background_single_image('./images/2021-02-16-085831.jpg', model))
-    plt.show()
+    cam = cv2.VideoCapture(0)
+    out_cam = pyfakewebcam.FakeWebcam('/dev/video2', *WEBCAM_OUTPUT_RESOLUTION)
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print('failure')
+            continue
+        result = remove_background_single_image(Image.fromarray(frame), model)
+        out_cam.schedule_frame(np.asarray(result))
+        time.sleep(1 / 30)
+    del cam
 
 
 if __name__ == '__main__':
